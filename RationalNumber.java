1e6f918df58b2b0128c935fa75dd0fca394adeac
hongweimachene <hchen00@stuy.edu>
2018-10-15 23:37:27 -0400

    Rational class

---
+++ RationalNumber.java
@@ -0,0 +1,11 @@
+public class RationalNumber extends RealNumber{
+  private int numerator, denominator;
+  public RationalNumber() {
+  }
+  /*
+  public double getN() {}
+  public String toString() {}
+  public int getNumer() {}
+  public int getDenomin() {}
+  */
+}
