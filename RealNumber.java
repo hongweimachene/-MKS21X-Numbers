a4923745d22d85526bfc16d7944fc6d025515325
hongweimachene <hchen00@stuy.edu>
2018-10-15 23:37:44 -0400

    Real class

---
+++ RealNumber.java
@@ -0,0 +1,15 @@
+public class RealNumber{
+  private double n;
+  public RealNumber() {
+    n = 10;
+  }
+  public RealNumber(double number) {
+    n = number;
+  }
+  public double getN() {
+    return n;
+  }
+  public String toString() {
+    return "";
+  }
+}
