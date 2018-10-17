6a83ad162e8faf39be06d1466061be83ea955d9d
hongweimachene <hchen00@stuy.edu>
2018-10-17 10:12:18 -0400

    Rational class

--- RationalNumber.java
+++ RationalNumber.java
@@ -1,8 +1,13 @@
 public class RationalNumber extends RealNumber{
   private int numerator, denominator;
   public RationalNumber(int nume, int deno) {
+    if (deno == 0) {
+      numerator = 0;
+      denominator = 1;
+    }
     numerator = nume;
     denominator = deno;
+    reduce();
   }
 
   public int getNumer() {
@@ -12,6 +17,7 @@ public class RationalNumber extends RealNumber{
   public int getDeno() {
     return denominator;
   }
+
   public RationalNumber reciprocal() {
     return new RationalNumber(denominator, numerator);
   }
@@ -21,10 +27,35 @@ public class RationalNumber extends RealNumber{
   }
 
   public double getN() {
-    return numerator / denominator;
+    return numerator / (double) denominator;
   }
 
   public String toString() {
     return "";
   }
+  private static int gcd(int a, int b) {
+    int s = 0;
+    int r = 1;
+    if (a < b) {
+      s = a;
+      a = b;
+      b = s;
+    }
+    while (r != 0) {
+      r = a % b;
+      if (r == 0){
+        return b;
+      }
+      a = b;
+      b = r;
+    }
+  }
+  private void reduce() {
+    int numer = getNumer();
+    numerator = numerator / gcd(numer, denominator);
+    denominator = denominator / gcd(numer, denominator);
+  }
+  
+
+
 }
